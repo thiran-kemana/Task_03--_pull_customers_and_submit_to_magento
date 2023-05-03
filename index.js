36bc1c79fab1dbf1478d3d2fc35f08003fd8e8c5
thiran-kemana <tsjayakody@kemana.com>
2023-05-03 10:11:48 +0530

    Update V0.1

---
+++ index.js
@@ -0,0 +1,62 @@
+const { createClient } = require("@commercetools/sdk-client");
+const {
+  createAuthMiddlewareForClientCredentialsFlow,
+} = require("@commercetools/sdk-middleware-auth");
+const { createHttpMiddleware } = require("@commercetools/sdk-middleware-http");
+
+const projectKey = "custom_project";
+const clientId = "HBPrIz0YM-nnO6PabxbRpLgR";
+const clientSecret = "4ref7KnLWpu1wH4wMpnzcZUCDhYLKBMn";
+const apiUrl = "https://api.australia-southeast1.gcp.commercetools.com";
+const authUrl = "https://auth.australia-southeast1.gcp.commercetools.com";
+
+const customerUrl = "https://commercetools.kemanamagento.web.id/customer";
+
+const authMiddleware = createAuthMiddlewareForClientCredentialsFlow({
+  host: authUrl,
+  projectKey,
+  credentials: {
+    clientId,
+    clientSecret,
+  },
+});
+
+const httpMiddleware = createHttpMiddleware({
+  host: apiUrl,
+});
+
+const client = createClient({
+  middlewares: [authMiddleware, httpMiddleware],
+});
+
+async function main() {
+  const fetchModule = await import("node-fetch");
+  const fetch = fetchModule.default;
+
+  client
+    .execute({
+      uri: `/${projectKey}/customers`,
+      method: "GET",
+    })
+    .then((result) => {
+      const customers = result.body.results;
+      // Log customers to console
+      console.log("Customers:", customers);
+      // Submit customers to customerUrl using POST method
+      return fetch(customerUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(customers),
+      });
+    })
+    .then((response) => {
+      console.log("Customers submitted successfully");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+main();
